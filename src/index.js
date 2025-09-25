a28d7f18cd5a11e142fb8241b7321ebcbbda8997
Fabian Martin <fabianmartinanz@gmail.com>
2025-09-25 12:19:48 -0500

    fix: desregistrar Service Workers y limpiar caches para mitigar problemas de versiones antiguas

--- src/index.js
+++ src/index.js
@@ -19,3 +19,25 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Mitigar clientes atascados en una versión vieja: desregistrar SW y limpiar caches.
+// Esto ayuda si en despliegues anteriores se registró un Service Worker (CRA antiguo) o
+// si quedaron caches de Workbox. No afecta si nunca hubo SW.
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations?.().then((regs) => {
+    regs.forEach((r) => r.unregister());
+    if (regs.length) {
+      console.info('[app] Service Workers desregistrados:', regs.length);
+    }
+  }).catch(() => {});
+}
+
+// Limpia caches de la app (scope del origen actual) para forzar a traer assets nuevos tras un deploy.
+// Nota: esto no borra almacenamiento de otras apps; solo caches del mismo origen.
+if (typeof caches !== 'undefined' && caches.keys) {
+  caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then((res) => {
+    if (Array.isArray(res) && res.some(Boolean)) {
+      console.info('[app] Caches limpiadas');
+    }
+  }).catch(() => {});
+}
